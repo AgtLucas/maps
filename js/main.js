8e1d8307446b8b4ae6e64380c3a4aac56ce92d99
Lucas <metallucas.ds@gmail.com>
2013-06-12 20:52:59 -0300

    New files

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-$(document).ready(function() {
-    function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("maps"),
-            mapOptions);
-    }
-});
-
 (function() {
 'use strict';
 function initialize() {
