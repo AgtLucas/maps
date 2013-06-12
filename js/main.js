e92007a861a343733f7a68cba20ee0964cc2c212
Lucas <metallucas.ds@gmail.com>
2013-06-12 20:44:43 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
+$(document).ready(function() {
+    function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("maps"),
+            mapOptions);
+    }
+});
+
+(function() {
+'use strict';
 function initialize() {
-var mapOptions = {
-  center: new google.maps.LatLng(-34.397, 150.644),
-  zoom: 8,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map(document.getElementById("maps"),
-    mapOptions);
-}
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("maps"),
+            mapOptions);
+    }
+})();
