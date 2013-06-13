e2dbede04329e2108c976fdc469b38b75ea75a01
Lucas <metallucas.ds@gmail.com>
2013-06-12 21:42:46 -0300

    Ahaha, I love JavaScript <3

--- js/main.js
+++ js/main.js
@@ -13,6 +13,33 @@ function initialize() {
         position: qth,
         map: map
     });
+
+    var boxText = document.createElement("div");
+    boxText.style.cssText = "border: 1px solid blakc; margin-top: 5px; background: tomato; padding: 5px";
+    boxText.innerHTML = "<p>This is a test...</p>";
+
+    var customOptions = {
+        content: boxText,
+        disableAutoPan: false,
+        maxWidth: 0,
+        pixelOffset: new google.maps.Size(-140, 0),
+        zIndex: null,
+        boxStyle: {
+            background: "red",
+            opacity: 0.75,
+            width: "280px"
+        },
+        infoBoxClearance: new google.maps.Size(1, 1),
+        isHidden: false,
+        pane: "floatPane",
+        enableEventPropagation: false
+    };
+
+    google.maps.event.addListener(marker, "click", function() {
+        ib.open(map, this)
+    });
+
+    var ib = new InfoBox(customOptions);
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
