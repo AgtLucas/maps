124539bd176c3950263c1e7a2eee6aada17ddda0
Lucas <metallucas.ds@gmail.com>
2013-06-12 21:33:26 -0300

    Updates..

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-function initializeMap() {
-var myOptions = {
-  center: new google.maps.LatLng(-34.397, 150.644),
-  zoom: 8,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
+function initialize() {
+    var qth = new google.maps.LatLng(-34.397, 150.644);
+    var myOptions = {
+        center: qth,
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(document.getElementById("maps"),
+        myOptions);
+
+    var marker = new google.maps.Marker({
+        position: qth,
+        map: map
+    });
 };
-var map = new google.maps.Map(document.getElementById("maps"),
-    myOptions);
-}
-initializeMap();
+
+google.maps.event.addDomListener(window, 'load', initialize);
